b9c5adb17f8937b1f374769d993c93565f7bfe0f
Unknown <dev@swoeste.de>
2017-12-12 12:08:52 +0100

    dummy implementation for creature sensor

---
+++ src/main/java/de/swoeste/demo/gen/alg/model/creature/sensor/vision/AbstractCreatureVisionSensor.java
@@ -0,0 +1,58 @@
+/*-
+ * Copyright (C) 2017 Sebastian Woeste
+ *
+ * Licensed to Sebastian Woeste under one or more contributor license
+ * agreements. See the NOTICE file distributed with this work for additional
+ * information regarding copyright ownership. I license this file to You under
+ * the Apache License, Version 2.0 (the "License"); you may not use this file
+ * except in compliance with the License. You may obtain a copy of the License
+ * at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package de.swoeste.demo.gen.alg.model.creature.sensor.vision;
+
+import de.swoeste.demo.gen.alg.model.Vector;
+import de.swoeste.demo.gen.alg.model.creature.Creature;
+import de.swoeste.demo.gen.alg.model.world.World;
+
+/**
+ * @author swoeste
+ */
+public abstract class AbstractCreatureVisionSensor extends AbstractVisionSensor {
+
+    public AbstractCreatureVisionSensor(final World world, final Creature creature) {
+        super(world, creature);
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    protected double getSensorValue(final World world, final Creature creature, final Vector position) {
+        return 0;
+        // final Vector visionPosition = getPosition(world, creature, position);
+
+        // final Rectangle detectionShape = getDetectionShape(world, creature, position);
+        //
+        // if (world.isPositionInWorld(visionPosition)) {
+        // final Tile tile = world.getTile(visionPosition);
+        // final int r = tile.getAttributeValue(TileAttribute.COLOR_R);
+        // final int g = tile.getAttributeValue(TileAttribute.COLOR_G);
+        // final int b = tile.getAttributeValue(TileAttribute.COLOR_B);
+        // final double value = getNumberRepresentation(r, g, b);
+        // // TODO logging debug?
+        // return value;
+        // } else {
+        // return 0.0;
+        // }
+    }
+
+    // protected abstract Rectangle getDetectionShape(final World world, final Creature creature, final Vector
+    // position);
+
+}
